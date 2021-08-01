fd2010bf1aa3575a2f5666eaf2768f0aeac2ef80
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-08-01 10:37:40 +0530

    welcome message is displayed

---
+++ src/com/message/Welcome.java
@@ -0,0 +1,8 @@
+package com.message;
+
+public class Welcome {
+	 public static void main(String[] args) {
+
+	     System.out.println("Welccome to line comparision Computation program");
+	     }
+}
